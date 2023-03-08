d80a9bfb8ab4571c1e096c544c9c2ba81cd6ba6c
Polgár András <azazdeaz@gmail.com>
2023-03-08 17:46:20 +0100

    relay odom and cmd_vel topic between Nav2 and Gazebo

--- sam_bot_description/launch/display.launch.py
+++ sam_bot_description/launch/display.launch.py
@@ -137,6 +137,32 @@ def generate_launch_description():
         output="screen",
     )
 
+    relay_odom = Node(
+        name="relay_odom",
+        package="topic_tools",
+        executable="relay",
+        parameters=[
+            {
+                "input_topic": "/diff_drive_base_controller/odom",
+                "output_topic": "/odom",
+            }
+        ],
+        output="screen",
+    )
+
+    relay_cmd_vel = Node(
+        name="relay_cmd_vel",
+        package="topic_tools",
+        executable="relay",
+        parameters=[
+            {
+                "input_topic": "/cmd_vel",
+                "output_topic": "/diff_drive_base_controller/cmd_vel_unstamped",
+            }
+        ],
+        output="screen",
+    )
+
     return launch.LaunchDescription(
         [
             DeclareLaunchArgument(
@@ -189,5 +215,7 @@ def generate_launch_description():
                     on_exit=[load_joint_trajectory_controller],
                 )
             ),
+            relay_odom,
+            relay_cmd_vel,
         ]
     )
