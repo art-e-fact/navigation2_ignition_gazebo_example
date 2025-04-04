e5986644fc03665646691376c77efece558e3248
Andras Polgar <azazdeaz@gmail.com>
2025-04-04 11:43:36 +0200

    Add rosbag recording

--- src/sam_bot_nav2_gz/test/test_follow_waypoints.py
+++ src/sam_bot_nav2_gz/test/test_follow_waypoints.py
@@ -1,4 +1,3 @@
-
 import os
 from datetime import datetime
 
@@ -16,7 +15,6 @@ from artefacts_toolkit.gazebo import gz
 from artefacts_toolkit.rosbag import image_topics, rosbag
 
 
-
 @pytest.fixture
 def rosbag_data():
     """Create and start a rosbag recorder for the test."""
@@ -31,7 +29,10 @@ def rosbag_data():
     rosbag_filepath = os.path.join("output", "rosbag2_" + yyyymmddhhmmss)
     rosbag_cmd = (
         ["ros2", "bag", "record"]
-        + topics + sim_topics + metrics + camera_topics
+        + topics
+        + sim_topics
+        + metrics
+        + camera_topics
         + ["-o", rosbag_filepath, "--storage", "mcap"]
     )
 
@@ -47,7 +48,7 @@ def rosbag_data():
         "/test_talker",
         "std_msgs/msg/String",
         "--",
-        "\"{data: 'Recording...'}\""
+        "\"{data: 'Recording...'}\"",
     ]
     print(f"Recording to {rosbag_filepath} with command: {' '.join(rosbag_cmd)}")
     return launch.actions.ExecuteProcess(
@@ -57,8 +58,9 @@ def rosbag_data():
         # cmd=test_talker_node_cmd,
         shell=True,
         cached_output=True,
-        output='both',
+        output="both",
     )
+
 
 @pytest.fixture(scope="module")
 def navigation_stack():
@@ -77,61 +79,102 @@ def navigation_stack():
         "run_headless:=True",
     ]
     print(f"Starting navigation stack with command: {' '.join(launch_cmd)}")
+
     return launch.actions.ExecuteProcess(
         name="navigation_stack",
         cmd=launch_cmd,
         shell=True,
         cached_output=True,
-        output='both',        
-        sigterm_timeout="600.0",  # Wait 30 seconds after SIGTERM before SIGKILL
+        output="both",
+        sigterm_timeout="600.0",
         sigkill_timeout="600.0",
     )
 
+@pytest.fixture(scope="module")
+def rosbag_recording():
+    topics = ["/odom"]
+    metrics = ["/distance_from_start_gt", "/distance_from_start_est", "/odometry_error"]
+    camera_topics = ["/sky_cam"]
+    sim_topics = ["/world/dynamic_pose/info"]
+    bag_recorder, rosbag_filepath = rosbag.get_bag_recorder(
+        topics + sim_topics + metrics + camera_topics, use_sim_time=True
+    )
+    return {
+        'bag_recorder': bag_recorder,
+        'rosbag_filepath': rosbag_filepath,
+    }
+
 # This function specifies the processes to be run for our test.
-@launch_pytest.fixture(scope="module") # Set the scope so the processes are not killed after each test
-def launch_description(navigation_stack):
-    return launch.LaunchDescription([
-        navigation_stack,
-        # Tell launch when to start the test
-        # If no ReadyToTest action is added, one will be appended automatically.
-        launch_pytest.actions.ReadyToTest(),
-        launch_testing.util.KeepAliveProc(),
-    ])
+@launch_pytest.fixture(
+    scope="module"
+)  # Set the scope so the processes are not killed after each test
+def launch_description(navigation_stack, rosbag_recording):
+    
+    return launch.LaunchDescription(
+        [
+            navigation_stack,
+            rosbag_recording["bag_recorder"],
+            # Tell launch when to start the test
+            # If no ReadyToTest action is added, one will be appended automatically.
+            launch_pytest.actions.ReadyToTest(),
+            launch_testing.util.KeepAliveProc(),
+        ]
+    )
+
 
 @pytest.mark.launch(fixture=launch_description)
 def test_nav2_ready(navigation_stack, launch_context):
     def validate_output(output):
-        assert "Nav2 is ready for use!" in output, 'process never printed "Nav2 is ready for use!"'
+        assert "Nav2 is ready for use!" in output, (
+            'process never printed "Nav2 is ready for use!"'
+        )
+
     process_tools.assert_output_sync(
-        launch_context, navigation_stack, validate_output, timeout=250)
+        launch_context, navigation_stack, validate_output, timeout=250
+    )
 
 
 @pytest.mark.launch(fixture=launch_description)
 def test_followed_waypoints(navigation_stack, launch_context):
     """Check if the robot followed the waypoints."""
 
-    def validate_output(output):\
+    def validate_output(output):
         assert "Goal succeeded!" in output, 'process never printed "Goal succeeded!"'
+
     process_tools.assert_output_sync(
-        launch_context, navigation_stack, validate_output, timeout=250)
-    
+        launch_context, navigation_stack, validate_output, timeout=250
+    )
+
+
 @pytest.mark.launch(fixture=launch_description)
 def test_no_skipped_waypoints(navigation_stack, launch_context):
     """Check if the robot followed the waypoints."""
 
     # TODO: figure out the number of waypoints
     def validate_output(output):
-        assert "Executing current waypoint: 1" in output, 'process never printed "Executing current waypoint: 1/1"'
+        assert "Executing current waypoint: 1" in output, (
+            'process never printed "Executing current waypoint: 1/1"'
+        )
+
     process_tools.assert_output_sync(
-        launch_context, navigation_stack, validate_output, timeout=250)
-    
+        launch_context, navigation_stack, validate_output, timeout=250
+    )
 
 
-@pytest.fixture(scope="session", autouse=True)
-def global_teardown(request):
+@pytest.fixture(scope="module", autouse=True)
+def generate_charts(request, rosbag_recording):
     """Run after all tests have completed."""
-    print("\n=== Running global teardown ===")
-    # This code runs before any tests
     yield
     # This code runs after all tests complete
-    print("\n=== Running global teardown ===")
+    print("Generating charts...")
+    rosbag_filepath = rosbag_recording["rosbag_filepath"]
+    print(rosbag_filepath)
+    make_chart(
+        rosbag_filepath,
+        "/odom.pose.pose.position.x",
+        "/odom.pose.pose.position.y",
+        field_unit="m",
+        chart_name="odometry_position",
+    )
+    image_topics.extract_camera_image(rosbag_filepath, "/sky_cam")
+    image_topics.extract_video(rosbag_filepath, "/sky_cam", "output/sky_cam.webm")
