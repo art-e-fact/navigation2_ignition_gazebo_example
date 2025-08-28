68660a8cdb1844b5b1fe8a934ecbfbddda47357c
Fabian D <fabian.dubois@gmail.com>
2025-08-28 17:18:16 +0900

    Tweak spawn position

--- src/sam_bot_nav2_gz/launch/display.launch.py
+++ src/sam_bot_nav2_gz/launch/display.launch.py
@@ -110,7 +110,7 @@ def generate_launch_description():
             "-topic",
             "robot_description",
             "-z",
-            "0.16",
+            "0.151",
             "-x",
             "-2.0",
             "--ros-args",
